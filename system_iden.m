019514a906b5e54f1b1e3c186a420f834e036be2
libing64 <libing64@gmail.com>
2019-03-03 16:38:01 +0800

    update system iden

--- system_iden.m
+++ system_iden.m
@@ -2,7 +2,7 @@
 load('matlab.mat');
 x = input_data.data;
 N = length(x);
-nfft = 2^nextpow2(N)
+nfft = 2^nextpow2(N);
 
 y = output_data.data(1:N);
 
@@ -17,25 +17,33 @@ plot(f, x);
 subplot(2, 1, 2);
 plot(f, y);
 
+
+N2 = N/2;
+valid_index = 1 : N2;
 figure(2);
-fn = (1 : nfft) / N * fs;
+fn = (1 : N2) / N * fs;
 fftx = windowing_fft(x);
+mag = abs(fftx(valid_index));
+phase = angle(fftx(valid_index)) * 180 / pi;
 title('fft analysis of input data')
 subplot(2, 1, 1);
-plot(fn, abs(fftx));
+plot(fn, mag);
 subplot(2, 1, 2);
-plot(fn, angle(fftx));
+plot(fn, phase);
 
 
 h = windowing_fft(y) ./ windowing_fft(x);
-mag = abs(h);
-phase = angle(h);
+mag = abs(h(valid_index));
+phase = angle(h(valid_index)) * 180 / pi;
 figure(3);
-title('system iden result')
+
 subplot(2, 1,1);
-plot(20 * log(fn), mag);
+title('system iden mag response')
+loglog(fn(valid_index), mag);
+
 subplot(2, 1, 2);
-plot(20 * log(fn), phase);
+title('system iden phase response')
+semilogx(fn, phase);
 
 figure(4);
 title('ideal bode plot');
--- windowing_fft.m
+++ windowing_fft.m
@@ -1,7 +1,7 @@
 function fftx = windowing_fft(x)
     N = length(x);
     nfft = 2^nextpow2(N)
-    win = hamming(N);
+    win = hanning(N);
     fftx = fft(win.*x, nfft);
 
 end
