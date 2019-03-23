a9165b41f6788c9be5139f4560d0ab03f84cd5bb
libing64 <libing64@gmail.com>
2019-03-24 00:00:28 +0800

    add result fig

--- system_iden.m
+++ system_iden.m
@@ -10,43 +10,50 @@ fs = 1000;
 t = (1 : N) / fs;
 
 f = (1 : N) / N * fs;
-figure(1);
-title('raw input and ouput data')
+fig1 = figure(1);
 subplot(2, 1, 1);
 plot(f, x);
+title('raw input data')
 subplot(2, 1, 2);
 plot(f, y);
+title('raw ouput data');
+saveas(fig1, 'raw_input_output.png')
 
 
 N2 = N/2;
 valid_index = 1 : N2;
-figure(2);
+fig2 = figure(2);
 fn = (1 : N2) / N * fs;
 fftx = windowing_fft(x);
 mag = abs(fftx(valid_index));
 phase = angle(fftx(valid_index)) * 180 / pi;
-title('fft analysis of input data')
 subplot(2, 1, 1);
 plot(fn, mag);
+title('fft analysis of input data, mag')
 subplot(2, 1, 2);
+title('fft analysis of input data, phase')
 plot(fn, phase);
+saveas(fig2, 'fft analysis of input data.png')
 
 
 h = windowing_fft(y) ./ windowing_fft(x);
 mag = abs(h(valid_index));
 phase = angle(h(valid_index)) * 180 / pi;
-figure(3);
+fig3 = figure(3);
 
 subplot(2, 1,1);
-title('system iden mag response')
 loglog(fn(valid_index), mag);
+title('system iden mag response')
 
 subplot(2, 1, 2);
-title('system iden phase response')
 semilogx(fn, phase);
+title('system iden phase response')
+saveas(fig3, 'system identification.png')
+
 
 figure(4);
-title('ideal bode plot');
 H = tf([1],[1, 0.2, 1]);
 bode(H);
+title('ideal bode plot');
+
 
